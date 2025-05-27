fa24f3d400058b0a11e19f157b8ad517ba55124f
L3tas <qwawswdwew@gmail.com>
2025-05-27 23:45:01 +0300

    Update job.js

--- job.js
+++ job.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://raw.githubusercontent.com/L3tas/twitch-jobs-list/refs/heads/main/jobs.txt');
+  const response = await fetch('https://raw.githubusercontent.com/L3tas/twitch-jobs-list/main/jobs.txt');
   const text = await response.text();
   const jobs = text.trim().split('\n');
   const randomJob = jobs[Math.floor(Math.random() * jobs.length)];
